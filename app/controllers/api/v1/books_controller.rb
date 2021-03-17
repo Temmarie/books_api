2d8071ebde25932815e53c1dce8fe5cbf2681f06
Temmarie <temmar_whyte16@yahoo.com>
2021-03-17 15:32:28 +0100

    Add books controllers and necessary methods

---
+++ app/controllers/api/v1/books_controller.rb
@@ -0,0 +1,34 @@
+class Api::V1::BooksController < ApplicationController
+  def index
+    books = Book.all
+    render json: books, status:200
+  end
+
+  def create
+    book = Book.new(
+    name: book_params[:name],
+    author: book_params[:author],
+    image: book_params[:image],
+    description: book_params[:description]
+    )
+    if book.save
+      render json: book, status: 200
+    else
+      render json: { error: "Error creating book."}
+    end
+  end
+
+  def show
+    book = Book.find_by(id: params[:id])
+    if book
+      render json: book, status: 200
+    else
+      render json; {error: "Book not found."}
+    end
+  end
+
+  private
+  def book_params
+    params.require(:book).permit([ :name, :author, :image, :description])
+  end
+end
