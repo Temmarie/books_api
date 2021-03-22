ad71ba82b0a0318b9073a260a186f75d0845c197
Temmarie <temmar_whyte16@yahoo.com>
2021-03-22 15:53:17 +0100

    Add devise to namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,20 @@
 Rails.application.routes.draw do
-  devise_for :users, path: '', path_names: {
-    sign_in: 'login',
-    sign_out: 'logout',
-    registration: 'signup'
-  },
-  controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations'
-  }
+  
 
   namespace :api, defaults: { format: :json }  do
     namespace :v1 do
      resources :books, only: [:index, :show, :create]
      get '/current_user', to: 'current_user#index'
      get 'private/test'
- 
+     devise_for :users, path: '', path_names: {
+      sign_in: 'login',
+      sign_out: 'logout',
+      registration: 'signup'
+    },
+    controllers: {
+      sessions: 'users/sessions',
+      registrations: 'users/registrations'
+    }
     end
   end
 end
