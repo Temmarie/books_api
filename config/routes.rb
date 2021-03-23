3cfcc62367f2a680e76b9863e4dd5ee72d385e90
Temmarie <temmar_whyte16@yahoo.com>
2021-03-23 16:16:57 +0100

    Secure user model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+
   namespace :api do
     namespace :v1 do
      resources :books, only: [:index, :show, :create]
+
+     resource :users, only: [:create]
+     post "/login", to: "users#login"
+     get "/auto_login", to: "users#auto_login"
     end
   end
 end
