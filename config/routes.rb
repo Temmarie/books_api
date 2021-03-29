09ade7f113a4a1c8c8c7f958c162f8305ea97017
Temmarie <temmar_whyte16@yahoo.com>
2021-03-29 11:03:13 +0200

    Add root to project

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  root 'books#index'
   namespace :api do
     namespace :v1 do
      resources :books, only: [:index, :show, :create]
-     resources :favs
+     resources :favs, only: [:index, :create]
      resource :users, only: [:create]
        post "/login", to: "users#login"
       get "/auto_login", to: "users#auto_login"
