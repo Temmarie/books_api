6b04e24541f8ae9fad577a6042507bfde8ab4c5d
Temmarie <temmar_whyte16@yahoo.com>
2021-03-29 15:04:26 +0200

    Generate Favourite books controller and auth controller

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -5,7 +5,7 @@ class AuthController < ApplicationController
       if user&.authenticate(params[:password])
         payload = { user_id: user.id }
         token = encode_token(payload)
-        render json: { user: user, jwt: token, fav_shoe: user.favourite_shoes, success: "Welcome back, #{user.username}" }
+        render json: { user: user, jwt: token, fav_book: user.favourite_books, success: "Welcome back, #{user.username}" }
       else
         render json: { failure: 'Log in failed! Username or password invalid!' }
       end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
     root 'books#index'
-  resources :books
+    resources :favs, only: [:index, :create]
+    get 'books', to: 'books#index'
+    get 'books/:id', to: 'books#show'
     post "/users", to: "users#create"
     post "/login", to: "auth#login"
 end
