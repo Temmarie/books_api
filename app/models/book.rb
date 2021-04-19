eb985d9d19661a6da0a3143fa07fa9117fe2e70a
Temmarie <temmar_whyte16@yahoo.com>
2021-04-19 16:52:35 +0200

    Make Reviewer suggested changes

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,8 @@
 class Book < ApplicationRecord
   has_many :favs
+
+  
+ validates :name, presence: true
+  validates :author, presence: true
+  validates :description, presence: true
 end
---
+++ spec/models/fave_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Fav, type: :model do
+
+  describe 'associations' do
+    it { should belong_to(:book) }
+    it { should belong_to(:user) }
+  end
+  describe 'validations' do
+    it { validate_uniqueness_of :book_id }
+  end
+end
