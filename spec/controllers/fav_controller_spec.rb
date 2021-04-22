5b747a3a489bc0d774abc26ef2d964f179eacb91
Temmarie <temmar_whyte16@yahoo.com>
2021-04-22 12:05:23 +0200

    FIx rubocop errors

--- spec/controllers/fav_controller_spec.rb
+++ spec/controllers/fav_controller_spec.rb
@@ -2,12 +2,11 @@ require 'rails_helper'
 
 RSpec.describe FavsController, type: :controller do
   it { should use_before_action(:authorized) }
-  
 
-  describe "GET index" do
-    it "gets books" do
-       get :index
-       expect(response).to render_template(nil)
+  describe 'GET index' do
+    it 'gets books' do
+      get :index
+      expect(response).to render_template(nil)
     end
   end
 end
--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -2,5 +2,4 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
   it { should use_before_action(:authorized) }
-  
 end
